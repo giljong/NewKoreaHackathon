34b4e1cae9167b3b062818d7b609410c4644cf7c
giljong <minjong5054@daum.net>
2019-07-19 22:06:53 +0900

    파싱 뺌

--- server.js
+++ server.js
@@ -3,13 +3,8 @@ const app = express();
 const request = require('request');
 const cheerio = require('cheerio');
 
-app.get('/',(req,res) =>{
-    request.get({url : "https://www.google.com/destination/map/topsights?tcfs=Ei0KCC9tLzA1YjdxEiHsobDshKDrr7zso7zso7zsnZjsnbjrr7zqs7XtmZTqta0&hl=ko&gl=kr&output=search&dest_mid=/m/05b7q&dest_mid=/m/05b7q&dest_mid=/m/05b7q"},(err,response,body) => {
-        const $ = cheerio.load(body);
-        const a = $("#ow3 > div.gws-travel-header__main-content.Npuxu > div.PKKBc.AfES6d.GmOjKf.r-iwYwQs7shZ40 > div.w9GWWb > div > ol > li:nth-child(2) > div > img").attr('src')
-        console.log(req);
+app.get('/tour/list',(req,res) =>{
+    let sample = [];
+    sample.push({path : path, url : url});
 
-    })
-    //let sample = [];
-    //sample.push({id : id, url : url, tag : tag, summary : summary});
 })
