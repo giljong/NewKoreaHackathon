f5b9f3634b800cb1a3887e8bd7be6b997fbf5d9b
daclouds <daclouds@gmail.com>
2019-07-19 20:07:57 +0900

    Add example: get photos

--- pages/main.vue
+++ pages/main.vue
@@ -1,11 +1,34 @@
 <template>
   <div>
-    <h1>Main!</h1>
+    <h1>Tourist Attraction!</h1>
+    <ul>
+      <li v-for="(post, index) in posts" :key="index">
+        <NuxtLink :to="{ name: 'posts-id', params: { id: post.id } }">
+          <img :src=post.thumbnailUrl />
+        </NuxtLink>
+      </li>
+    </ul>
   </div>
 </template>
 
+<style>
+  ul li {
+    display: inline-block;
+    margin: 0 8px;
+  }
+</style>
+
 <script>
+import axios from 'axios'
+
 export default {
+  asyncData ({ req, params }) {
+    // We can return a Promise instead of calling the callback
+    return axios.get('https://jsonplaceholder.typicode.com/photos')
+      .then((res) => {
+        return { posts: res.data.slice(0, 5) }
+      })
+  },
   head: {
     title: 'Main page'
   }
